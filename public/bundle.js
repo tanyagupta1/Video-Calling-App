77ccc81e0e6d9e46425a0ed527c706b89af31faf
tanyagupta1 <55709526+tanyagupta1@users.noreply.github.com>
2021-07-13 21:30:29 +0530

    Update bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -2,42 +2,41 @@
 const socket = io('/') // Create our socket
 const videoGrid = document.getElementById('video-grid') // Find the Video-Grid element
 
-const myPeer = new Peer() // Creating a peer element which represents the current user
-const myVideo = document.createElement('video') // Create a new video tag to show our video
-myVideo.muted = true // Mute ourselves on our end so there is no feedback loop
+const myPeer = new Peer() 
+const myVideo = document.createElement('video')
+myVideo.muted = true 
 
-// Access the user's video and audio
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    addVideoStream(myVideo, stream) // Display our video to ourselves
+    addVideoStream(myVideo, stream)
 
-    myPeer.on('call', call => { // When we join someone's room we will receive a call from them
-        call.answer(stream) // Stream them our video/audio
-        const video = document.createElement('video') // Create a video tag for them
-        call.on('stream', userVideoStream => { // When we recieve their stream
-            addVideoStream(video, userVideoStream) // Display their video to ourselves
+    myPeer.on('call', call => { 
+        call.answer(stream) 
+        const video = document.createElement('video') 
+        call.on('stream', userVideoStream => { 
+            addVideoStream(video, userVideoStream) 
         })
     })
 
-    socket.on('user-connected', userId => { // If a new user connect
+    socket.on('user-connected', userId => { 
         connectToNewUser(userId, stream) 
     })
 })
 
-myPeer.on('open', id => { // When we first open the app, have us join a room
+myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
 
-function connectToNewUser(userId, stream) { // This runs when someone joins our room
-    const call = myPeer.call(userId, stream) // Call the user who just joined
-    // Add their video
+function connectToNewUser(userId, stream) { 
+    const call = myPeer.call(userId, stream)
+ 
     const video = document.createElement('video') 
     call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream)
     })
-    // If they leave, remove their video
+    
     call.on('close', () => {
         video.remove()
     })
@@ -46,7 +45,7 @@ function connectToNewUser(userId, stream) { // This runs when someone joins our 
 
 function addVideoStream(video, stream) {
     video.srcObject = stream 
-    video.addEventListener('loadedmetadata', () => { // Play the video as it loads
+    video.addEventListener('loadedmetadata', () => { 
         video.play()
     })
     videoGrid.append(video) 
